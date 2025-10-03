8d06e14aa799f6f3bfa883b75b570d289aeeacee
Aswin s <achu92706@gmail.com>
2025-10-03 10:02:40 +0530

    Time: 22 ms (40.8%), Space: 72.5 MB (78.23%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    let set=new Set(nums)
+     if(nums.length!==set.size){
+        return true
+     }else{
+        return false
+     }
+};
