4977175d29d244ac3a24e5b4dab5c3bd33f5d3d0
Aswin s <achu92706@gmail.com>
2025-10-28 18:31:04 +0530

    Time: 0 ms (100%), Space: 58.1 MB (5.03%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.js
@@ -0,0 +1,12 @@
+
+var addDigits = function (num) {
+    let str = num.toString()
+    let sum = 0
+    for (let i = 0; i < str.length; i++) {
+        sum += Number(str[i])
+    } if (sum > 9) {
+        let val = sum.toString()
+        return addDigits(val)
+    }
+    return sum
+};
