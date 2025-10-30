e88efbdc0f58bcd1d24c81593a5f739a022dd8ac
Aswin s <achu92706@gmail.com>
2025-10-30 16:33:21 +0530

    Time: 2 ms (70.47%), Space: 56.6 MB (91.32%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.js
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.js
@@ -1,14 +1,12 @@
-/**
- * @param {number[]} nums
- * @return {number[]}
- */
+
 var sortArrayByParity = function(nums) {
-    let a=nums.length 
-    for (let i=0;i<a;i++){
-        if(nums[i]%2==0){
-            let a=nums.pop(nums[i])
-            nums.unshift(a)
-        }
-    }
-    return nums
+    // let a=nums.length 
+    // for (let i=0;i<a;i++){
+    //     if(nums[i]%2==0){
+    //         let a=nums.pop(nums[i])
+    //         nums.unshift(a)
+    //     }
+    // }
+    // return nums
+     return nums.filter(x => x % 2 === 0).concat(nums.filter(x => x % 2 !== 0));
 };
