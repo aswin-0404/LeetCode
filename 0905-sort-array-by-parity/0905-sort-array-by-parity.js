240ebaa8a797183b4870ce0410540ed7a0145659
Aswin s <achu92706@gmail.com>
2025-10-30 16:33:16 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortArrayByParity = function(nums) {
+    let a=nums.length 
+    for (let i=0;i<a;i++){
+        if(nums[i]%2==0){
+            let a=nums.pop(nums[i])
+            nums.unshift(a)
+        }
+    }
+    return nums
+};
