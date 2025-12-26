ebf39063066b9707f7cf4bdea8b01cb71686183e
Aswin s <achu92706@gmail.com>
2025-12-26 13:21:27 +0530

    Time: 3 ms (48.4%), Space: 12.5 MB (51.02%) - LeetHub

---
+++ 3467-transform-array-by-parity/3467-transform-array-by-parity.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def transformArray(self, nums):
+        for i in range(len(nums)):
+            if nums[i]%2==0:
+                nums[i]=0
+            else:
+                nums[i]=1
+        nums.sort() 
+        return nums    
