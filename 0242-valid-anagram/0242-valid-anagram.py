04ed9b789bd12c0b9d9a3e7526308385541e3209
Aswin s <achu92706@gmail.com>
2026-01-15 00:14:47 +0530

    Time: 23 ms (50.61%), Space: 13.6 MB (22.6%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        ss=sorted(s)
+        tt=sorted(t)
+        if ss == tt:
+            return True
+        else:
+            return False
