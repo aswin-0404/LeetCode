9811203322294d7ea53082ccc6338a8c0e93939d
Aswin s <achu92706@gmail.com>
2026-01-28 14:53:26 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.py
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.py
@@ -1,11 +1,10 @@
 class Solution(object):
     def checkIfExist(self, arr):
-        count=0
         for ind,i in enumerate(arr):
-            if i*2 in arr:
-                if ind != arr.index(i*2):
-                    count+=1
-        if count>0:
-            return True
-        else:
-            return False
+            for jind,j in enumerate(arr):
+                if ind!=jind and i*2 in arr:
+                    return True
+        return False
+
+
+         
