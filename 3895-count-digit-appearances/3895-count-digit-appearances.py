bc738875a39dbdd52742f9cd73e048ca75799a56
Aswin s <achu92706@gmail.com>
2026-07-03 10:09:23 +0530

    Time: 23 ms (57.79%), Space: 19.5 MB (52.6%) - LeetHub

--- 3895-count-digit-appearances/3895-count-digit-appearances.py
+++ 3895-count-digit-appearances/3895-count-digit-appearances.py
@@ -2,8 +2,10 @@ class Solution:
     def countDigitOccurrences(self, nums: list[int], digit: int) -> int:
         count=0
 
+        digit = str(digit)
+
         for i in nums:
             for j in str(i):
-                if int(j) == digit:
+                if j == digit:
                     count+=1
         return count
